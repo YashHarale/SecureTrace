e430ad0ac34ce4ba7208f7739608f95806d5ead8
YashHarale <yashharale2003@gmail.com>
2024-03-25 18:54:29 +0530

    It worked again

--- app.js
+++ app.js
@@ -1,113 +1,112 @@
 var contractAbi =
-	[
-		{
-			"anonymous": false,
-			"inputs": [
-				{
-					"indexed": true,
-					"internalType": "uint256",
-					"name": "productId",
-					"type": "uint256"
-				}
-			],
-			"name": "Added",
-			"type": "event"
-		},
-		{
-			"inputs": [
-				{
-					"internalType": "uint256",
-					"name": "_productId",
-					"type": "uint256"
-				},
-				{
-					"internalType": "string",
-					"name": "info",
-					"type": "string"
-				}
-			],
-			"name": "addState",
-			"outputs": [
-				{
-					"internalType": "string",
-					"name": "",
-					"type": "string"
-				}
-			],
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"inputs": [
-				{
-					"internalType": "string",
-					"name": "_text",
-					"type": "string"
-				},
-				{
-					"internalType": "string",
-					"name": "_date",
-					"type": "string"
-				}
-			],
-			"name": "newItem",
-			"outputs": [
-				{
-					"internalType": "bool",
-					"name": "",
-					"type": "bool"
-				}
-			],
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"inputs": [
-				{
-					"internalType": "string",
-					"name": "_a",
-					"type": "string"
-				},
-				{
-					"internalType": "string",
-					"name": "_b",
-					"type": "string"
-				}
-			],
-			"name": "concat",
-			"outputs": [
-				{
-					"internalType": "string",
-					"name": "",
-					"type": "string"
-				}
-			],
-			"stateMutability": "pure",
-			"type": "function"
-		},
-		{
-			"inputs": [
-				{
-					"internalType": "uint256",
-					"name": "_productId",
-					"type": "uint256"
-				}
-			],
-			"name": "searchProduct",
-			"outputs": [
-				{
-					"internalType": "string",
-					"name": "",
-					"type": "string"
-				}
-			],
-			"stateMutability": "view",
-			"type": "function"
-		}
-	]
-
+[
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "productId",
+				"type": "uint256"
+			}
+		],
+		"name": "Added",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "info",
+				"type": "string"
+			}
+		],
+		"name": "addState",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_text",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_date",
+				"type": "string"
+			}
+		],
+		"name": "newItem",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_a",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_b",
+				"type": "string"
+			}
+		],
+		"name": "concat",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			}
+		],
+		"name": "searchProduct",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
  // Paste Contract Here
- var contractAddress = '0xBDc1Ca61FfeF48A39fE84EcbceE7a252Cf194e83';
+ var contractAddress = '0x0cd2115C0AeE876397a6Fb6CFC57Ae6a98b70f0C';
 
 // May be the problem is up with ABI
 //Usually contract address starts with "0x" that is why this problem, as for our prev contract address i.e  '0x73CF39Fb50a0F3E1f4cbAB2ebf6c0C13fdBEBe65' the account address was '0xE something'
